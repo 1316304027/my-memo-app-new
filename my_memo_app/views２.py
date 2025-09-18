24432c5928e6a1d8873629e721bd189a373a47b7
1316304027 <1316304027@qq.com>
2025-09-18 09:54:34 +0900

    final: add all project files for deployment

---
+++ my_memo_app/views２.py
@@ -0,0 +1,207 @@
+from flask import render_template, request, redirect, url_for, flash
+from app import app
+from models import db, Memo, User
+from forms import MemoForm, LoginForm, SignUpForm
+from flask_login import login_user, logout_user
+
+#======================================================================================
+# ルーティング
+#======================================================================================
+#▼▼▼▼▼ リスト11.3追加修正版 ▼▼▼▼▼
+# ログイン(Form使用)
+@app.route("/",methods=["GET","POST"])
+def login():
+    #Formインストール生成
+    form = LoginForm()
+    if form.validate_on_submit():
+        #データ入力取得
+        username = form.username.data
+        password = form.password.data
+        #
+        user = User.query.filter_by(username=username).first()
+        # 認証判定
+        if user is not None and user.check_password(password):
+            # 成功
+            # 引数として渡されたuserオブジェクトを使用して、ユーザーをログイン状態にする
+            login_user(user)
+            #
+            return redirect(url_for("index"))
+        # 失敗
+        flash("認証不備です")
+    # GET時
+    #
+    return render_template("login_form.html",form=form)
+
+#ログアウト
+@app.route("/logout")
+def logout():
+    # 現在ログインしているユーザーをログアウトする
+    logout_user()
+    # フラッシュメッセージ
+    flash("ログアウトしました")
+    #
+    return redirect(url_for("login"))
+
+#サインアップ(Form使用)
+@app.route("/register",methods=["GET","POST"])
+def register():
+    #Formインストール生成
+    form = SignUpForm()
+    if form.validate_on_submit():
+        #データ入力取得
+        username = form.username.data
+        password = form.password.data
+        #
+        user = User(username=username)
+        #
+        user.set_password(password)
+        #登録処理
+        db.session.add(user)
+        db.session.commit()
+        #　フラッシュメッセージ
+        flash("ユーザー登録しました")
+        #
+        return redirect(url_for("login"))
+    # GET時
+    #
+    return render_template("register_form.html",form=form)
+
+#▲▲▲▲▲ リスト11.3追加修正版 ▲▲▲▲▲
+
+# 一覧
+@app.route("/memo/")
+def index():
+    #メモ全件取得
+    memos = Memo.query.all()
+    #画面遷移
+    return render_template("index.html",memos=memos)
+
+#▼▼▼▼▼ リスト10.6追加修正版 ▼▼▼▼▼
+
+# 登録(Form使用)
+@app.route("/momo/create",methods=["GET","POST"])
+def create():
+    # Formインストール生成
+    form = MemoForm()
+    if form.validate_on_submit():
+        # データ入力取得
+        title = form.title.data
+        content = form.content.data
+        #登録処理
+        memo=Memo(title=title,content=content)
+        db.session.add(memo)
+        db.session.commit()
+        # フラッシュメッセージ
+        flash("登録しました")
+        #画面遷移
+        return redirect(url_for("index"))
+    # GET
+    # 画面遷移
+    return render_template("create_form.html",form = form)
+
+#更新(Form使用)
+@app.route("/momo/update/<int:memo_id>",methods=["GET","POST"])
+def update(memo_id):
+    # データベースからmemo_idに一致するメモを取得し、
+    # 見つからない場合は404エラーを表示
+    target_data = Memo.query.get_or_404(memo_id)
+    # Form に入れ替え
+    form = MemoForm(obj=target_data)
+
+    if request.method=="POST" and form.validate():
+        # 変更処理
+        target_data.title = form.title.data
+        target_data.content = form.content.data
+        db.session.commit()
+        # フラッシュメッセージ
+        flash("変更しました")
+        # 画面遷移
+        return redirect(url_for("index"))
+    # GET時
+    # 画面遷移
+    return render_template("update_form.html", form=form , edit_id =target_data.id)
+
+#　削除
+@app.route("/memo/delete/<int:memo_id>")
+def delete(memo_id):
+    # データベースからmemo_idに一致するメモを取得し、
+    # 見つからない場合は404エラーを表示
+    memo = Memo.query.get_or_404(memo_id)
+    # 削除処理
+    db.session.delete(memo)
+    db.session.commit()
+    # フラッシュメッセージ
+    flash("削除しました")
+    # 画面遷移
+    return redirect(url_for("index"))
+#▲▲▲▲▲ リスト10.6追加修正版 ▲▲▲▲▲
+
+# モジュールのインポート
+from werkzeug.exceptions import NotFound
+
+#エラーハンドリング
+@app.errorhandler(NotFound)
+def show_404_page(error):
+    msg=error.description
+    print('エラー内容：',msg)
+    return render_template('errors/404.html',msg=msg), 404
+
+
+"""
+@app.route("/momo/create",methods=["GET","POST"])
+def create():
+    # PSOT時
+    if request.method =="POST":
+        # データ入力取得
+        title = request.form["title"]
+        content = request.form["content"]
+        #登録処理
+        memo=Memo(title=title,content=content)
+        db.session.add(memo)
+        db.session.commit()
+        return redirect(url_for("index"))
+    #GET時
+    #画面遷移
+    return render_template("create.html")
+
+#更新
+@app.route("/momo/update/<int:memo_id>",methods=["GET","POST"])
+def update(memo_id):
+    # データベースからmemo_idに一致するメモを取得し、
+    # 見つからない場合は404エラーを表示
+    memo = Memo.query.get_or_404(memo_id)
+    # POST時
+    if request.method=="POST":
+        # 変更処理
+        memo.title = request.form["title"]
+        memo.content = request.form["content"]
+        db.session.commit()
+        # 画面遷移
+        return redirect(url_for("index"))
+    # GET時
+    # 画面遷移
+    return render_template("update.html", memo=memo)
+
+#　削除
+@app.route("/memo/delete/<int:memo_id>")
+def delete(memo_id):
+    # データベースからmemo_idに一致するメモを取得し、
+    # 見つからない場合は404エラーを表示
+    memo = Memo.query.get_or_404(memo_id)
+    # 削除処理
+    db.session.delete(memo)
+    db.session.commit()
+    # 画面遷移
+    return render_template(url_for("index"))
+
+# モジュールのインポート
+from werkzeug.exceptions import NotFound
+
+#エラーハンドリング
+@app.errorhandler(NotFound)
+def show_404_page(error):
+    msg=error.description
+    print('エラー内容：',msg)
+    return render_template('errors/404.html',msg=msg), 404
+
+"""
